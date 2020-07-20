4bd425675eecc4bdb1caa1a12fc0f2e62aeeccf0
Prasanna-ChennaiGameStudio <68425963+Prasanna-ChennaiGameStudio@users.noreply.github.com>
2020-07-20 10:24:00 +0530

    Script Added

---
+++ SixthProject_Master/Assets/Scripts/Testing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    float incrementalCount;
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        Debug.Log("incrementalCount" + incrementalCount++);
+    }
+}
